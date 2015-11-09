372eb488a436338f3179ec64ac8baab42396c723
Gaston Galera <lucianogaston.galera@arris.com>
2015-11-09 16:33:21 -0300

    updating structure

---
+++ test/functional/Vendor/Features/ProviderPlaceService.php
@@ -0,0 +1,77 @@
+<?php 
+
+namespace Vendor\Features;
+
+class ProviderPlaceService
+{
+    private $RestClient;
+    private $jsonResponse;
+
+    public function __construct()
+    {
+        $this->RestClient = new RestClient($this->RestClient);
+    }
+
+    public function ExistProviderPlaceId($providerName, $providerPlaceId, $path)
+    {
+        $this->getProviderPlaceId($providerName, $providerPlaceId, $path);
+        if ($this->jsonResponse['meta']['code'] == 200) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public function deleteProviderPlaceId($providerName, $providerPlaceId, $path)
+    {
+        $deleteResponse = $this->RestClient->rest_call("DELETE", "$path/$providerName/$providerPlaceId");
+        $this->jsonResponse = json_decode($deleteResponse, true);
+
+        return $this->jsonResponse;
+    }
+
+    public function createProviderPlaceId($providerName, $providerPlaceId, $path)
+    {
+        $createResponse = $this->RestClient->rest_call("PUT", "$path/$providerName/$providerPlaceId");
+        $this->jsonResponse = json_decode($createResponse, true);
+
+        return $this->jsonResponse;
+    }
+
+    public function getProviderPlaceId($providerName, $providerPlaceId, $path)
+    {
+        $getResponseUUID = $this->RestClient->rest_call("GET", "$path/$providerName/$providerPlaceId");
+        $this->jsonResponse = json_decode($getResponseUUID, true);
+
+        return $this->jsonResponse;
+    }
+
+    public function getProviderPlaceIdWithUUID($uuid, $path)
+    {
+        $getResponse = $this->RestClient->rest_call("GET", "$path/$uuid");
+        $this->jsonResponse = json_decode($getResponse, true);
+
+        return $this->jsonResponse;
+    }
+
+    public function statusCodeOk($response)
+    {
+        if ($response['meta']['code'] != 200) {
+            throw new Exception("An error occurs; code: $response->meta->code, error: $response->meta->error ");
+        }
+    }
+
+    public function statusCodeCreated($response)
+    {
+        if ($response['meta']['code'] != 201) {
+            throw new Exception("An error occurs on creation; code: $response->meta->code, error: $response->meta->error");
+        }
+    }
+
+    public function statusCodeNotFound($response)
+    {
+        if ($response['meta']['code'] != 404) {
+            throw new Exception("An error occurs on search; code: $response->meta->code, error: $response->meta->error");
+        }
+    }
+}
---
+++ test/functional/Vendor/Features/RestClient.php
@@ -0,0 +1,77 @@
+<?php 
+
+namespace Vendor\Features;
+
+class RestClient
+{
+    private $response;
+    private $options;
+
+    public function __construct()
+    {
+        $this->options = $this->generic_options();
+    }
+
+    public function rest_call($method, $url)
+    {
+        switch ($method) {
+            case "PUT":
+                $this->put_rest_call("$url");
+                break;
+            case "GET":
+                $this->get_rest_call("$url");
+                break;
+            case "DELETE":
+                $this->delete_rest_call("$url");
+                break;
+            default:
+                throw new Exception("method not allowed. $method");
+        }
+
+        return $this->response;
+    }
+
+    private function get_rest_call($url)
+    {
+        $this->options[CURLOPT_CUSTOMREQUEST] = "GET";
+        $this->sendRequest($url);
+    }
+
+    private function put_rest_call($url)
+    {
+        $this->options[CURLOPT_CUSTOMREQUEST] = "PUT";
+        $this->sendRequest($url);
+    }
+
+    private function delete_rest_call($url)
+    {
+        $this->options[CURLOPT_CUSTOMREQUEST] = "DELETE";
+        $this->sendRequest($url);
+    }
+
+    private function sendRequest($url)
+    {
+        $curlSession = curl_init($url);
+        curl_setopt_array($curlSession, $this->options);
+        $this->response = curl_exec($curlSession);
+        curl_close($curlSession);
+    }
+
+    private function generic_options()
+    {
+        $options = array(
+            CURLOPT_RETURNTRANSFER => true,   // return web page
+            CURLOPT_HEADER => false,  // return headers
+            CURLOPT_FOLLOWLOCATION => false,  // follow redirects
+            CURLOPT_MAXREDIRS => 10,     // stop after 10 redirects
+            CURLOPT_ENCODING => "",     // handle compressed
+            CURLOPT_USERAGENT => "test", // name of client
+            CURLOPT_AUTOREFERER => true,   // set referrer on redirect
+            CURLOPT_CONNECTTIMEOUT => 120,    // time-out on connect
+            CURLOPT_TIMEOUT => 120,    // time-out on response
+        );
+
+        return $options;
+    }
+
+}
